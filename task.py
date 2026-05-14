cbe958907f937c300475dd65e57030ff9b3a4bdd
Rahimakhanim1 <55282174+Rahimakhanim1@users.noreply.github.com>
2026-05-14 16:02:57 +0400

    Add tasks for number input and name greeting

--- task.py
+++ task.py
@@ -1,1 +1,19 @@
 
+# Task 1
+number = int(input("Enter a number: "))
+if number > 7:
+  print("Hello")
+
+#Task 2
+name = input("Enter a name: ")
+if name == "John":
+  print("Hello, John")
+else:
+  print("There is no such name")
+
+#Task 3
+numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+print("Numbers divisible by 3")
+for num in numbers:
+  if num % 3 == 0:
+    print(num)
